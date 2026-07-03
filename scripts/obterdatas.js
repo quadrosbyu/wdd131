db33b69fd702afe3cd399a1128bd578b3b5a792d
Marcelo Quadros <quadroscontabilidadecrcrs@gmail.com>
2026-07-03 13:58:55 -0300

    alt22

---
+++ scripts/obterdatas.js
@@ -0,0 +1,10 @@
+/ Seleciona os elementos do rodapé pelos seus respectivos IDs
+const yearSpan = document.getElementById("currentyear");
+const lastModifiedSpan = document.getElementById("lastModified");
+
+// Obtém o ano atual dinamicamente e injeta no primeiro parágrafo
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+// Obtém a data/hora da última modificação do arquivo e injeta no segundo parágrafo
+lastModifiedSpan.textContent = document.lastModified;

1f634f023ae0a754acdf3068b1a775aa2c4991fe
Marcelo Quadros <quadroscontabilidadecrcrs@gmail.com>
2026-07-22 22:01:43 -0300

    ALT96

--- scripts/templos-filtrados.js
+++ scripts/templos-filtrados.js
@@ -5,70 +5,70 @@ const templos = [
     localizacao: "Aba, Nigéria",
     consagracao: "2005, 7 de agosto",
     area: 11500,
-    urlDaImagem: "https://churchofjesuschrist.org"
+    urlDaImagem: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
   },
   {
     nomeDoTemplo: "Manti Utah",
     localizacao: "Manti, Utah, Estados Unidos",
     consagracao: "1888, 21 de maio",
     area: 74792,
-    urlDaImagem: "https://churchofjesuschrist.org"
+    urlDaImagem: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-utah-temple-lds-273998-wallpaper.jpg"
   },
   {
     nomeDoTemplo: "Payson Utah",
     localizacao: "Payson, Utah, Estados Unidos",
     consagracao: "2015, 7 de junho",
     area: 96630,
-    urlDaImagem: "https://churchofjesuschrist.org"
+    urlDaImagem: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
   },
   {
     nomeDoTemplo: "Yigo Guam",
     localizacao: "Yigo, Guam",
     consagracao: "2020, 2 de maio",
     area: 6861,
-    urlDaImagem: "https://churchofjesuschrist.org"
+    urlDaImagem: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo-guam-temple-lds-273997-wallpaper.jpg"
   },
   {
     nomeDoTemplo: "Washington D.C.",
     localizacao: "Kensington, Maryland, Estados Unidos",
     consagracao: "1974, 19 de novembro",
     area: 156558,
-    urlDaImagem: "https://churchofjesuschrist.org"
+    urlDaImagem: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
   },
   {
     nomeDoTemplo: "Lima Peru",
     localizacao: "Lima, Peru",
     consagracao: "1986, 10 de janeiro",
     area: 9600,
-    urlDaImagem: "https://churchofjesuschrist.org"
+    urlDaImagem: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-lds-273996-wallpaper.jpg"
   },
   {
     nomeDoTemplo: "Cidade do México, México",
     localizacao: "Cidade do México, México",
     consagracao: "1983, 2 de dezembro",
     area: 116642,
-    urlDaImagem: "https://churchofjesuschrist.org"
+    urlDaImagem: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   },
   {
     nomeDoTemplo: "Curitiba Brasil",
     localizacao: "Curitiba, Paraná, Brasil",
     consagracao: "2008, 1 de junho",
     area: 27850,
-    urlDaImagem: "https://churchofjesuschrist.org"
+    urlDaImagem: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/curitiba-brasil/400x250/curitiba-brasil-temple-lds-273995-wallpaper.jpg"
   },
   {
     nomeDoTemplo: "Frankfurt Alemanha",
     localizacao: "Friedrichsdorf, Alemanha",
     consagracao: "1987, 28 de agosto",
     area: 32895,
-    urlDaImagem: "https://churchofjesuschrist.org"
+    urlDaImagem: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/frankfurt-alemanha/400x250/frankfurt-alemanha-temple-lds-273994-wallpaper.jpg"
   },
   {
     nomeDoTemplo: "Paris França",
     localizacao: "Le Chesnay, França",
     consagracao: "2017, 21 de maio",
     area: 44175,
-    urlDaImagem: "https://churchofjesuschrist.org"
+    urlDaImagem: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/paris-franca/400x250/paris-franca-temple-lds-273993-wallpaper.jpg"
   }
 ];
 
